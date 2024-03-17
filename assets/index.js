9afee0f1c3ca2d8a363b623bdfd5801bee5596b5
Harsh Gupta <110103967+witharsh@users.noreply.github.com>
2024-03-17 21:31:46 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+var togglebtn= document.getElementById('toogle-btn');
+var toggleBody =document.getElementById("toggle-body");
+
+function toggleMenuOn(){
+toggleBody.style.display="block";
+}
+function toggleMenuOff(){
+toggleBody.style.display="none";
+}
